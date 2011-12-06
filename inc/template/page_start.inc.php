84ef2b5450f5697404a6e61ebf9369879bae3ecb
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-06 16:08:08 +0330

    SEO: Added Meta tags to page_start.inc.php

--- inc/template/page_start.inc.php
+++ inc/template/page_start.inc.php
@@ -1,15 +1,21 @@
-﻿﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+﻿﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		
-		<title>Jahaan Pardazesh Alborz</title>
+		<meta name="description" content="<?php echo $this->page['description']; ?>" />
+		<meta name="keywords" content="<?php echo $this->page['keywords']; ?>" />
+		
+		<title>Jahaan Pardazesh Alborz :: <?php echo $this->page['title']; ?></title>
 		
 		<style type="text/css" media="screen">
+		<![CDATA[
 			@import "/inc/stylesheet/font.css";
 			@import "/inc/stylesheet/general.css";
 			@import "/inc/stylesheet/background.css";
 			@import "/inc/stylesheet/layout.css";
+		]]>
 		</style>
 		
 		<script src="inc/scripts/mootools.js"></script>
