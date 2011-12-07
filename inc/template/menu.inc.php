3f17e11ebb01b9f0100d88b57e4b1291be14d77c
mehdi <mehdi@ubuntu.ubuntu-domain>
2011-12-07 14:08:19 +0330

    change template

--- inc/template/menu.inc.php
+++ inc/template/menu.inc.php
@@ -1,27 +1,27 @@
 		<ul id="menu-wrapper" class="menu-hover menu-hover-text">
 			<li id="home" class="menu-items menu-top-item-light-bg cursor-pointer ">
 				<a href="#" class="menu-items-links display-block menu-items-font">
-					<span class="menu-items-text menu-items-text-hover">Home</span>
+					<span class="menu-items-text menu-items-text-hover display-none">Home</span>
 				</a>
 			</li>
 			<li id="services" class="menu-items menu-middle-items-light-bg cursor-pointer">
 				<a href="#" class="menu-items-links display-block menu-items-font">
-					<span class="menu-items-text menu-items-text-hover">Services</span>
+					<span class="menu-items-text menu-items-text-hover display-none">Services</span>
 				</a>
 			</li>
 			<li id="online-order" class="menu-items menu-middle-items-light-bg cursor-pointer">
 				<a href="#" class="menu-items-links display-block menu-items-font">
-					<span class="menu-items-text menu-items-text-hover">Online&nbsp;order</span>
+					<span class="menu-items-text menu-items-text-hover display-none">Online&nbsp;order</span>
 				</a>
 			</li>
 			<li id="about" class="menu-items menu-middle-items-light-bg cursor-pointer">
 				<a href="#" class="menu-items-links display-block menu-items-font">
-					<span class="menu-items-text menu-items-text-hover">About</span>
+					<span class="menu-items-text menu-items-text-hover display-none">About</span>
 				</a>
 			</li>
 			<li id="contact" class="menu-items menu-bottom-item-light-bg cursor-pointer">
 				<a href="#" class="menu-items-links display-block menu-items-font">
-					<span class="menu-items-text menu-items-text-hover">Contact</span>
+					<span class="menu-items-text menu-items-text-hover display-none">Contact</span>
 				</a>
 			</li>
 		</ul>
--- inc/template/portfolio.inc.php
+++ inc/template/portfolio.inc.php
@@ -1,9 +1,16 @@
 		<div id="sample-works-wrapper">
-			<div id="sample-work-1" class="cursor-move sample-work-1-bg">&nbsp;</div>
-			<div id="sample-work-2" class="cursor-move sample-work-2-bg">&nbsp;</div>
-			<div id="sample-work-3" class="cursor-move sample-work-3-bg">&nbsp;</div>
-			<div id="sample-work-4" class="cursor-move sample-work-4-bg">&nbsp;</div>
-			<div id="sample-work-5" class="cursor-move sample-work-5-bg">&nbsp;</div>
-			<div id="sample-work-6" class="cursor-move sample-work-6-bg">&nbsp;</div>
+
+			<div id="sample-work-4" class="cursor-move sample-work-4-bg sample-work-hover sample-work-main">&nbsp;
+				<div id="puzzle-block" class="puzzle-hide">
+							<div id="sample-work-1" class="cursor-move sample-work-1-bg sample-work-child">&nbsp;</div>
+							<div id="sample-work-2" class="cursor-move sample-work-2-bg sample-work-child">&nbsp;</div>
+							<div id="sample-work-3" class="cursor-move sample-work-3-bg sample-work-child">&nbsp;</div>
+							<div id="sample-work-5" class="cursor-move sample-work-5-bg sample-work-child">&nbsp;</div>
+							<div id="sample-work-6" class="cursor-move sample-work-6-bg sample-work-child">&nbsp;</div>
+				</div>
+			</div>
+
 		</div>
 
+
+<div id="drag-here" class="display-none"></div>
