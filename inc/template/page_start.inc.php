2d6400fdc4087c74b14574732d78a56503068e99
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-07 16:07:35 +0330

    Restored removed meta tags and title

--- inc/template/page_start.inc.php
+++ inc/template/page_start.inc.php
@@ -1,9 +1,13 @@
-﻿﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		
-		<title>Jahaan Pardazesh Alborz</title>
+		<meta name="description" content="<?php echo $this->page['description']; ?>" />
+		<meta name="keywords" content="<?php echo $this->page['keywords']; ?>" />
+		
+		<title>Jahaan Pardazesh Alborz :: <?php echo $this->page['title']; ?></title>
 		
 		<style type="text/css" media="screen">
 			@import "/inc/stylesheet/font.css";
@@ -11,12 +15,11 @@
 			@import "/inc/stylesheet/background.css";
 			@import "/inc/stylesheet/layout.css";
 		</style>
-
-		<script type="text/javascript" src="inc/scripts/lib/prototype.js"></script> 
 		
+		<script type="text/javascript" src="inc/scripts/lib/prototype.js"></script>
 		<script type="text/javascript" src="inc/scripts/src/scriptaculous.js"></script>
-		
-	 	<script type="text/javascript" src="inc/scripts/banner-menu-motion.js"></script> 
+	 	<script type="text/javascript" src="inc/scripts/banner-menu-motion.js"></script>
+	 	 
 	</head>
 
 	<body class="body-bg">
