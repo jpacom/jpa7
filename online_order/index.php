2b60b0b8c376996eccdfd38fd054ac215987d131
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-07 12:50:06 +0330

    Added: folders and static pages.

---
+++ online_order/index.php
@@ -0,0 +1,9 @@
+<?php
+	
+	$include_path	= $_SERVER['DOCUMENT_ROOT'] . '/inc/';
+	
+	require_once	$include_path . 'jpa_classes.inc.php';
+	
+	new StaticPage(false, 'online_order', false, 'en');
+	
+?>
