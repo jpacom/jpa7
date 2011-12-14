bf9c8d8ae8505b768e4e42fa80db333eecc7b712
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-14 10:35:20 +0330

    Changed method of banner hover to absolute position which enables us to impliment the banner motion class.

--- inc/template/banner.inc.php
+++ inc/template/banner.inc.php
@@ -1,4 +1,9 @@
-		<div id="banner-wrapper">
-                    <div id="banner" class="banner cursor-pointer banner-bg banner-hover">&nbsp;</div>
+		<div id="banner-wrapper" class="banner-hover">
+			<div id="banner-wrapper2">
+                    <div class="banner cursor-pointer banner-bg">&nbsp;</div>
+                    <div class="banner cursor-pointer banner-bg">&nbsp;</div>
+                    <div class="banner cursor-pointer banner-bg">&nbsp;</div>
+                    <div class="banner cursor-pointer banner-bg">&nbsp;</div>
+			</div>
 		</div>
 
