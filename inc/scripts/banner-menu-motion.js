33b41cc034f38ee48e94f5cafc01dbeb35afc64f
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-22 10:57:28 +0330

    Fixed replaced banner motion.

--- inc/scripts/banner-menu-motion.js
+++ inc/scripts/banner-menu-motion.js
@@ -5,6 +5,7 @@ var drag_effect = null;
 
 document.observe('dom:loaded', function() {
 
+	var banner = new Banner('banner-wrapper2', 4, 0.5);
 	
 	$('banner-wrapper').removeClassName('banner-hover');
     var banner_effect = null;
