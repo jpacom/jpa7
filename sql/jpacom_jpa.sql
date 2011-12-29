0b4400746e6d8da5241746112c6550a5f31ea37e
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2011-12-29 13:40:44 +0330

    Merged jpacom_portfolio DB to jpacom_jpa

--- sql/jpacom_jpa.sql
+++ sql/jpacom_jpa.sql
@@ -1,34 +1,63 @@
 -- phpMyAdmin SQL Dump
--- version 2.7.0-pl2
+-- version 3.2.4
 -- http://www.phpmyadmin.net
--- 
+--
 -- Host: localhost
--- Generation Time: Dec 07, 2011 at 11:58 AM
--- Server version: 5.0.18
--- PHP Version: 5.1.1
--- 
+-- Generation Time: Dec 29, 2011 at 10:09 AM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
 -- Database: `jpacom_jpa`
--- 
+--
 
 -- --------------------------------------------------------
 
--- 
--- Table structure for table `statics_en`
--- 
+--
+-- Table structure for table `portfolio`
+--
 
-CREATE TABLE `statics_en` (
-  `id` int(11) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `portfolio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `start` int(11) NOT NULL COMMENT 'timestamp',
+  `end` int(11) NOT NULL COMMENT 'timestamp',
+  `departmentId` int(11) NOT NULL,
+  `packId` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `puzzle_id` text NOT NULL,
+  `site_url` text NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statics_en`
+--
+
+CREATE TABLE IF NOT EXISTS `statics_en` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `title` text NOT NULL,
   `body` text NOT NULL,
   `description` text NOT NULL,
   `keywords` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
--- 
+--
 -- Dumping data for table `statics_en`
--- 
+--
 
-INSERT INTO `statics_en` VALUES (1, 'Welcome', 'JPA', '							<h1 class="H1-black-font"><span class="font-title-big-black-font">W</span>elcome to Jahan Pardazesh Alborz official website!</h1>\r\n							<h2 class="H2-black-font">Web design is the process of planning and creating a website. Text, images, digital media and interactive elements are shaped by the web designer to produce the page seen on the web browser.[1] Web designers utilize markup language, most notably HTML for structure and CSS for presentation to develop pages that can be read by web browsers.As a whole, the process of web design includes conceptualization, planning, post-production, research, advertising as well as media control that is applied to the pages within the site by the designer or group of designers with a specific purpose. The site itself can be divided into its main page, also known as the home page, which cites the main objective as well as highlights of the site''s daily updates; which also contains hyperlinks that functions to direct viewers to a designated page within the site''s domain.</h2>\r\n', '', '');
-INSERT INTO `statics_en` VALUES (2, 'Services-brief', 'Services-brief', '							<h1 class="H1-white-font"><span class="font-title-big-white-font">W</span>elcome to Jahan Pardazesh Alborz official website!</h1>\r\n							<h3 class="H3-white-font">Web design is the process of planning and creating a website. Text, images, digital media and interactive elements are shaped by the web designer to produce the page seen on the web browser.[1] Web designers utilize markup language, most notably HTML for structure and CSS for presentation to develop pages that can be read by web browsers.As a whole, the process of web design includes conceptualization, planning, post-production, research, advertising as well as media control that is applied to the pages within the site by the designer or group of designers with a specific purpose. The site itself can be divided into its main page, also known as the home page, which cites the main objective as well as highlights of the site''s daily updates; which also contains hyperlinks that functions to direct viewers to a designated page within the site''s domain.</h3>\r\n	', '', '');
+INSERT INTO `statics_en` (`id`, `name`, `title`, `body`, `description`, `keywords`) VALUES
+(1, 'Welcome', 'JPA', '							<h1 class="H1-black-font"><span class="font-title-big-black-font">W</span>elcome to Jahan Pardazesh Alborz official website!</h1>\r\n							<h2 class="H2-black-font">Web design is the process of planning and creating a website. Text, images, digital media and interactive elements are shaped by the web designer to produce the page seen on the web browser.[1] Web designers utilize markup language, most notably HTML for structure and CSS for presentation to develop pages that can be read by web browsers.As a whole, the process of web design includes conceptualization, planning, post-production, research, advertising as well as media control that is applied to the pages within the site by the designer or group of designers with a specific purpose. The site itself can be divided into its main page, also known as the home page, which cites the main objective as well as highlights of the site''s daily updates; which also contains hyperlinks that functions to direct viewers to a designated page within the site''s domain.</h2>\r\n', '', ''),
+(2, 'Services-brief', 'Services-brief', '							<h1 class="H1-white-font"><span class="font-title-big-white-font">W</span>elcome to Jahan Pardazesh Alborz official website!</h1>\r\n							<h3 class="H3-white-font">Web design is the process of planning and creating a website. Text, images, digital media and interactive elements are shaped by the web designer to produce the page seen on the web browser.[1] Web designers utilize markup language, most notably HTML for structure and CSS for presentation to develop pages that can be read by web browsers.As a whole, the process of web design includes conceptualization, planning, post-production, research, advertising as well as media control that is applied to the pages within the site by the designer or group of designers with a specific purpose. The site itself can be divided into its main page, also known as the home page, which cites the main objective as well as highlights of the site''s daily updates; which also contains hyperlinks that functions to direct viewers to a designated page within the site''s domain.</h3>\r\n	', '', '');
