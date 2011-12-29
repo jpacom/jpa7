d08195528d33de1f4ca17b92dc665828c665a8db
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2011-12-29 11:31:12 +0330

    css - html

--- inc/template/menu.inc.php
+++ inc/template/menu.inc.php
@@ -1,25 +1,25 @@
 		<ul id="menu-wrapper" class="menu-hover menu-hover-text">
-			<li id="home" class="menu-items menu-top-item-light-bg cursor-pointer ">
+			<li id="home" class="menu-items menu-home-item-light-bg cursor-pointer ">
 				<a href="/" class="menu-items-links display-block menu-items-font">
 					<span class="menu-items-text menu-items-text-hover display-none"><span class="display-none">H</span>ome</span>
 				</a>
 			</li>
-			<li id="services" class="menu-items menu-middle-items-light-bg cursor-pointer">
+			<li id="services" class="menu-items menu-services-items-light-bg cursor-pointer">
 				<a href="/services" class="menu-items-links display-block menu-items-font">
 					<span class="menu-items-text menu-items-text-hover display-none"><span class="display-none">S</span>ervices</span>
 				</a>
 			</li>
-			<li id="online-order" class="menu-items menu-middle-items-light-bg cursor-pointer">
+			<li id="online-order" class="menu-items menu-online-order-items-light-bg cursor-pointer">
 				<a href="/online_order" class="menu-items-links display-block menu-items-font">
 					<span class="menu-items-text menu-items-text-hover display-none"><span class="display-none">O</span>nline&nbsp;order</span>
 				</a>
 			</li>
-			<li id="about" class="menu-items menu-middle-items-light-bg cursor-pointer">
+			<li id="about" class="menu-items menu-about-items-light-bg cursor-pointer">
 				<a href="/about" class="menu-items-links display-block menu-items-font">
 					<span class="menu-items-text menu-items-text-hover display-none"><span class="display-none">A</span>bout</span>
 				</a>
 			</li>
-			<li id="contact" class="menu-items menu-bottom-item-light-bg cursor-pointer">
+			<li id="contact" class="menu-items menu-contact-item-light-bg cursor-pointer">
 				<a href="/contact" class="menu-items-links display-block menu-items-font">
 					<span class="menu-items-text menu-items-text-hover display-none"><span class="display-none">C</span>ontact</span>
 				</a>
