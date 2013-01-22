4fbe28cfa27385f6f176cf513b451d15fb97105a
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-01-22 22:37:50 +0330

    Remove the comming soon message and fix webiste home links.

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-		echo "Comming soon...";
-		// Testing repository. local
-		// Testing repository. online
+	
+	$home = true;
+	
+	$include_path	= $_SERVER['DOCUMENT_ROOT'] . '/inc/';
+	
+	require_once	$include_path . 'jpa_classes.inc.php';
+	
+	//	Initialize 1st page.
+	$page	= new StaticPage(1, false, false, 'en', false, 1);
+	
+	//	Header
+	$page->pageHeader();
+	
+	//	Box1
+	$page->pageBody();
+	
+	require_once $include_path . 'template/box2.inc.php';
+	
+	//	Footer
+	$page->pageFooter();
+	
 ?>
