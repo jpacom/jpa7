5b8e82e2b70716bd53e8e8eea4a46c74ca7db825
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-01-26 15:27:46 +0330

    Remove old page folders and create new ones.

--- webdesign/index.php
+++ webdesign/index.php
@@ -1,9 +1,10 @@
 <?php
-	
+
+	$language_id = 1;	
 	$include_path	= $_SERVER['DOCUMENT_ROOT'] . '/inc/';
 	
 	require_once	$include_path . 'jpa_classes.inc.php';
 	
-	new StaticPage(false, 'services', false, 'en');
+	new StaticPage(false, 'webdesign', false, 'en');
 	
 ?>
