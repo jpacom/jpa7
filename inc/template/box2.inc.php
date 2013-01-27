a091c20d5b209e7287e6026ba43ea42e72db5f93
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-01-27 10:32:52 +0330

    changing sample works position and fixed them. changing box2 from 4 boxes to 3 boxes

--- inc/template/box2.inc.php
+++ inc/template/box2.inc.php
@@ -62,27 +62,6 @@
 						<br class="clear-both" />
 					</div>
 				</div>
-				<div class="text-box-4-wrapper float-left">
-					<div class="text-box-top-wrapper">
-						<div class="text-box-top-left text-box-2-top-left-bg float-left">&nbsp;</div>
-						<div class="text-box-top-right text-box-2-top-right-bg float-right">&nbsp;</div>
-						<div class="text-box-top-center text-box-2-top-middle-bg">&nbsp;</div>
-					</div>
-					<div class="text-box-middle-left text-box-2-middle-left-bg clear-both">
-						<div class="text-box-middle-right text-box-2-middle-right-bg">
-							<div class="text-box-middle-center text-box-2-middle-center-bg">
-								<h1 class="H1-white-font"><span class="font-title-big-white-font">W</span>elcome to Jahan Pardazesh Alborz official website!</h1>
-								<p>Web designers utilize markup language, most notably HTML for structure and CSS for presentation to develop pages that can be read by web browsers.As a whole, the process of web design includes conceptualization, planning, post-production, research, advertising as well as media control that is applied to the pages within the site by the designer or group of designers with a specific purpose.</p>
-							</div>
-						</div>
-					</div>
-					<div class="text-box-bottom-wrapper">
-						<div class="text-box-top-left text-box-top-bottom text-box-2-bottom-left-bg float-left">&nbsp;</div>
-						<div class="text-box-top-right text-box-top-bottom text-box-2-bottom-right-bg float-right">&nbsp;</div>
-						<div class="text-box-top-center text-box-top-bottom text-box-2-bottom-middle-bg">&nbsp;</div>
-						<br class="clear-both" />
-					</div>
-				</div>
 				<br class="clear-both" />
 			</div>
 								
--- inc/template/page_start.inc.php
+++ inc/template/page_start.inc.php
@@ -1,6 +1,26 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="<?php
+	try {
+		$link = mysqli_connect(ConstsImp::db_host, ConstsImp::db_user, ConstsImp::db_pass);
+		mysqli_set_charset($link, 'utf8');
+		mysqli_select_db($link, ConstsImp::menu_items_db);
+		if(! $language_id)
+			$language_id = 1;
+		$query = "SELECT `abbr`, `direction` FROM `languages` WHERE `id`=" . $language_id;
+		$result = mysqli_query($link, $query);
+		$lang = mysqli_fetch_array($result);
+		$lang_abbr = $lang['abbr'];
+		$direction = $lang['direction'];
+		$lang = Null;
+		mysqli_free_result($result);
+		mysqli_close($link);
+		
+		echo $direction;
+	} catch (Exception $e) {
+		echo 'ltr';
+	}
+?>">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		
