cb41291ea3c0be4b879dcee0bff45699654e2306
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-01-27 23:17:56 +0330

    Add the missing banner2.

---
+++ inc/template/banner2.inc.php
@@ -0,0 +1,7 @@
+		<div id="banner2-wrapper" class="banner2-hover">
+			<div id="banner2-wrapper2">
+                    <div class="banner2 cursor-pointer banner2-bg" style="background-image:url('/banner/2/1.jpg');">&nbsp;</div>
+                    <div class="banner2 cursor-pointer banner2-bg" style="background-image:url('/banner/2/2.jpg');">&nbsp;</div>
+                    <div class="banner2 cursor-pointer banner2-bg" style="background-image:url('/banner/2/3.jpg');">&nbsp;</div>
+			</div>
+		</div>
