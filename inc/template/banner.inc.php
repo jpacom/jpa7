04bb9e628df002ea5cecda11f73c82727a2a07c5
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-04-27 23:19:20 +0430

    Remove previous version

--- inc/template/banner.inc.php
+++
@@ -1,9 +0,0 @@
-		<div id="banner-wrapper" class="banner-hover">
-			<div id="banner-wrapper2">
-                    <div class="banner cursor-pointer banner-bg" style="background-image:url('/banner/1.png');">&nbsp;</div>
-                    <div class="banner cursor-pointer banner-bg" style="background-image:url('/banner/2.png');">&nbsp;</div>
-                    <div class="banner cursor-pointer banner-bg" style="background-image:url('/banner/3.png');">&nbsp;</div>
-			</div>
-		</div>
-
-
