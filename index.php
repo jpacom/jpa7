6ee0e08e8cdaec5cfb83a0fecd483eaf7e5cbfd1
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-04-28 10:09:44 +0430

    Add css initial version.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Jahan Pardazesh Alborz</title>
+	<style type="text/css" media="screen">
+		@import "/inc/stylesheet/global.css";
+	</style>
+</head>
+
+<body>
+	<div id="language-bar">
+    	<div>
+        	<a href="#" class="language-bar-active">ENGLISH</a>
+            <span class="language-bar-deactive">|</span>
+        	<a href="#" class="language-bar-deactive">PERSIAN</a>
+        </div>
+    </div>
+    <div id="header">
+    	<div>&nbsp;</div>
+    </div>
+    <div id="banner">
+    	<div>
+        	<div class="banner-text">
+            	<a href="#">hhgf jhh hgfgf hjgh</a>
+            </div>
+    		<div id="banner-navigation">&nbsp;</div>
+        </div>
+    </div>
+    <div id="main">
+    	<div>&nbsp;</div>
+    </div>
+    <div id="who-are-our-customers">&nbsp;</div>
+    <div id="what-our-customers-say-about-us">&nbsp;</div>
+    <div id="our-latest-projects">&nbsp;</div>
+    <div id="what-can-you-do">&nbsp;</div>
+    <div id="footer">&nbsp;</div>
+</body>
+</html>
