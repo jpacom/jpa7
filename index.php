0c356a3f6f54e217fab6e17b981c2a14d92b242b
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-05-12 10:26:02 +0430

    The first Hagar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Jahan Pardazesh Alborz</title>
 	<style type="text/css" media="screen">
-		@import "/inc/stylesheet/global.css";
+		@import "inc/stylesheet/global.css";
 	</style>
 </head>
 
@@ -17,22 +17,159 @@
         </div>
     </div>
     <div id="header">
-    	<div>&nbsp;</div>
+    	<div>
+        	<div class="float-right">
+            	<div>
+                	<div class="float-right">
+                    	<div class="header-button-l float-left cursor-pointer">&nbsp;</div>
+                    	<div class="header-button-m float-left cursor-pointer">
+                        	<div>
+                        		<a href="#">Customer Login</a>
+                        	</div>
+                        </div>
+                    	<div class="header-button-r float-left cursor-pointer">&nbsp;</div>
+                    	<br class="clear-both" />
+                    </div>
+                	<div class="float-right">
+                    	<div class="header-button-l float-left cursor-pointer">&nbsp;</div>
+                    	<div class="header-button-m float-left cursor-pointer">
+                        	<a href="#">Order a katalog</a>
+                        </div>
+                    	<div class="header-button-r float-left cursor-pointer">&nbsp;</div>
+                    	<br class="clear-both" />
+                    </div>
+                	<div id="header-call-us" class="float-left">Call Us: +98 21 888&nbsp;88&nbsp;88&nbsp;88</div>
+                    <br class="clear-both" />
+                </div>
+                <div>satre 2 menu menu menu</div>
+            </div>
+            <br class="clear-both" />
+        </div>
     </div>
     <div id="banner">
     	<div>
         	<div class="banner-text">
-            	<a href="#">hhgf jhh hgfgf hjgh</a>
+            	<a href="#" class="banner-text-title">DUIS AUTE IRURE DOLOR INHE</a>
+            	<a href="#" class="banner-text-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</a>
             </div>
-    		<div id="banner-navigation">&nbsp;</div>
+        	<div class="banner-button banner-text">
+            	<div class="float-right banner-button-r">&nbsp;</div>
+            	<div class="float-right banner-button-m">Learn More</div>
+            	<div class="float-right banner-button-l">&nbsp;</div>
+                <br class="clear-both" />
+            </div>
+    		<div id="banner-navigation">
+            	<div>
+                    <div class="float-left banner-nav-active">&nbsp;</div>
+                    <div class="float-left banner-nav-deactive">&nbsp;</div>
+                    <div class="float-left banner-nav-deactive">&nbsp;</div>
+                    <div class="float-left banner-nav-deactive">&nbsp;</div>
+                    <div class="float-left banner-nav-deactive">&nbsp;</div>
+                    <br class="clear-both" />
+                </div>
+            </div>
         </div>
     </div>
     <div id="main">
-    	<div>&nbsp;</div>
+    	<div id="main-text">
+        	<div>
+            	<div class="main-text-title">Duis aute irure dolor in reprehenderit in voluptate velit</div>
+            	<div class="main-text-description display-inline">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                <div class="main-show-more-button display-inline">Show More</div>
+                <div class="main-point">
+                	<div class="float-right learn-more-point">&nbsp;</div>
+                	<div class="float-right learn-more-point">&nbsp;</div>
+                	<div class="float-right learn-more-point learn-more-point-end">&nbsp;</div>
+                    <br class="clear-both" />
+                </div>
+                <div id="products">
+                	<div class="float-left products-network">
+                    	<div class="products-title">Network</div>
+                    	<div class="products-subtitle">Management</div>
+                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                        <div class="products-learn-more">
+                        	<div class="float-left products-learn-more-l">&nbsp;</div>
+                        	<div class="float-left products-learn-more-m">Learn more</div>
+                        	<div class="float-left products-learn-more-r">&nbsp;</div>
+                            <br class="clear-both" />
+                        </div>
+                    </div>
+                    <div class="float-left products-programing">
+                    	<div class="products-title">Programing</div>
+                    	<div class="products-subtitle">Software development</div>
+                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                        <div class="products-learn-more">
+                        	<div class="float-left products-learn-more-l">&nbsp;</div>
+                        	<div class="float-left products-learn-more-m">Learn more</div>
+                        	<div class="float-left products-learn-more-r">&nbsp;</div>
+                            <br class="clear-both" />
+                        </div>
+                    </div>
+                    <div class="float-left products-hosting">
+                    	<div class="products-title">Hosting</div>
+                    	<div class="products-subtitle">Web and Applications</div>
+                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                        <div class="products-learn-more">
+                        	<div class="float-left products-learn-more-l">&nbsp;</div>
+                        	<div class="float-left products-learn-more-m">Learn more</div>
+                        	<div class="float-left products-learn-more-r">&nbsp;</div>
+                            <br class="clear-both" />
+                        </div>
+                    </div>
+                    <br class="clear-both" />
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="who-are-our-customers">&nbsp;</div>
-    <div id="what-our-customers-say-about-us">&nbsp;</div>
-    <div id="our-latest-projects">&nbsp;</div>
+    <div id="main-bottom">&nbsp;</div>
+    <div id="who-are-our-customers">
+    	<div>
+    		<div class="title">Who are our customers</div>
+        	<div class="description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        	<div class="box">
+                <div class="float-left" style="background-image:url(images/logo/logo-42.png); background-repeat:no-repeat; width:52px; height:61px; margin-top:10px; margin-left:35px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-41.png); background-repeat:no-repeat; width:76px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-40.png); background-repeat:no-repeat; width:57px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-39.png); background-repeat:no-repeat; width:57px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-38.png); background-repeat:no-repeat; width:52px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-37.png); background-repeat:no-repeat; width:85px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-36.png); background-repeat:no-repeat; width:80px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/logo-35.png); background-repeat:no-repeat; width:135px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <br class="clear-both" />
+        	</div>
+		</div>
+    </div>
+    <div id="what-our-customers-say-about-us">
+    	<div>
+    		<div class="title">What our customers say about us</div>
+            <div class="box">
+            	<div class="float-left">&nbsp;</div>
+            	<div class="float-right">&nbsp;</div>
+                <br class="clear-both" />
+            </div>
+		</div>
+    </div>
+    <div id="our-latest-projects">
+    	<div class="header">
+    		<div class="title float-left">Our latest project</div>
+            <div class="menu float-right">
+            	<div class="float-left active">Network</div>
+            	<div class="float-left deactive">Programing</div>
+            	<div class="float-left deactive">Hosting</div>
+                <br class="clear-both" />
+            </div>
+            <br class="clear-both" />
+        </div>
+        <div id="our-latest-projects-box" class="box">
+            <div class="float-left">&nbsp;</div>
+            <div class="float-left">&nbsp;</div>
+            <div class="float-left">&nbsp;</div>
+            <div class="float-left">&nbsp;</div>
+            <br class="clear-both" />
+        </div>
+    </div>
     <div id="what-can-you-do">&nbsp;</div>
     <div id="footer">&nbsp;</div>
 </body>
