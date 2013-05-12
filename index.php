c722bd4ac2466991ce95b195d8b536a806512137
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-05-12 23:11:02 +0430

    completing the "what can you do" part without checking details.

--- index.php
+++ index.php
@@ -177,7 +177,47 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
 	        <br />
         </div>
     </div>
-    <div id="what-can-you-do">&nbsp;</div>
+    <div id="what-can-you-do">
+    	<div class="header">
+    		<div class="right float-left">
+    			<div class="title">What can you do</div>
+    			<div class="description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+    		</div>
+    		<div class="social float-right">
+				<div class="title">Follow us around the web</div>
+				<div>
+					<div id="facebook" class="float-left icon">&nbsp;</div>
+					<div class="float-left text">exercitation ullamco laboris nisi</div>
+	            	<br class="clear-both" />
+				</div>
+				<div>
+					<div id="twitter" class="float-left icon">&nbsp;</div>
+					<div class="float-left text">exercitation ullamco laboris nisi</div>
+	            	<br class="clear-both" />
+				</div>
+				<div>
+					<div id="googleplus" class="float-left icon">&nbsp;</div>
+					<div class="float-left text">exercitation ullamco laboris nisi</div>
+	            	<br class="clear-both" />
+				</div>
+				<div>
+					<div id="youtube" class="float-left icon">&nbsp;</div>
+					<div class="float-left text">exercitation ullamco laboris nisi</div>
+	            	<br class="clear-both" />
+				</div>
+				<div>
+					<div id="rss" class="float-left icon">&nbsp;</div>
+					<div class="float-left text">exercitation ullamco laboris nisi</div>
+	            	<br class="clear-both" />
+				</div>
+			</div>
+            <br class="clear-both" />
+    	</div>
+    </div>
     <div id="footer">&nbsp;</div>
 </body>
 </html>
