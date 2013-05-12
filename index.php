d80bd5479d89fce9543693311a788e15863dbf54
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-05-12 16:23:22 +0430

    adding our latest project css

--- index.php
+++ index.php
@@ -161,13 +161,20 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
                 <br class="clear-both" />
             </div>
             <br class="clear-both" />
-        </div>
-        <div id="our-latest-projects-box" class="box">
-            <div class="float-left">&nbsp;</div>
-            <div class="float-left">&nbsp;</div>
-            <div class="float-left">&nbsp;</div>
-            <div class="float-left">&nbsp;</div>
-            <br class="clear-both" />
+	        <div id="our-latest-projects-box" class="box">
+	            <div class="no-margin float-left">&nbsp;</div>
+	            <div class="float-left">&nbsp;</div>
+	            <div class="float-left">&nbsp;</div>
+	            <div class="float-left">&nbsp;</div>
+	            <br class="clear-both" />
+	        </div>
+	        <div id="our-latest-projects-slider">
+	        	<div class="float-left left">&nbsp;</div>
+	        	<div class="float-left middle">&nbsp;</div>
+	        	<div class="float-left right">&nbsp;</div>
+	            <br class="clear-both" />
+	        </div>
+	        <br />
         </div>
     </div>
     <div id="what-can-you-do">&nbsp;</div>
