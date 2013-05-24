ff1cd8a590b00798dd9a53f4a57ecd15167c9b89
Hajar Sharpasand <hajar.sharpasand@gmail.com>
2013-05-24 10:58:45 +0430

    making index file for sticking splited files!

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/page-start.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/language-bar.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/header.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/banner.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/main.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/who-are-our-customers.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/what-our-customers-say-about-us.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/our-latest-projects.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/what-can-you-do.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/footer.inc.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/inc/template/page-end.inc.php";
+?>
