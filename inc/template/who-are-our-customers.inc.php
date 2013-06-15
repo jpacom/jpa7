316699675e33b764e8d921fa6d2923ffd82e62d3
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-06-15 19:58:17 +0430

    Add Our customers' logo. (partially)

--- inc/template/who-are-our-customers.inc.php
+++ inc/template/who-are-our-customers.inc.php
@@ -4,12 +4,11 @@
         	<div class="description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
         	<div class="box">
-                <div class="float-left" style="background-image:url(images/logo/logo-42.png); background-repeat:no-repeat; width:52px; height:61px; margin-top:10px; margin-left:35px;">&nbsp;</div>
-                <div class="float-left" style="background-image:url(images/logo/logo-41.png); background-repeat:no-repeat; width:76px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
-                <div class="float-left" style="background-image:url(images/logo/logo-40.png); background-repeat:no-repeat; width:57px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
-                <div class="float-left" style="background-image:url(images/logo/logo-39.png); background-repeat:no-repeat; width:57px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/RowshanSanat.png); background-repeat:no-repeat; width:76px; height:61px; margin-top:10px; margin-left:23px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/sampad.png); background-repeat:no-repeat; width:109px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/Ferdowsie.png); background-repeat:no-repeat; width:56px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
+                <div class="float-left" style="background-image:url(images/logo/HotelCanary.png); background-repeat:no-repeat; width:133px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
                 <div class="float-left" style="background-image:url(images/logo/logo-38.png); background-repeat:no-repeat; width:52px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
-                <div class="float-left" style="background-image:url(images/logo/logo-37.png); background-repeat:no-repeat; width:85px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
                 <div class="float-left" style="background-image:url(images/logo/logo-36.png); background-repeat:no-repeat; width:80px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
                 <div class="float-left" style="background-image:url(images/logo/logo-35.png); background-repeat:no-repeat; width:135px; height:61px; margin-top:10px; margin-left:32px;">&nbsp;</div>
                 <br class="clear-both" />
