e93e59ca3dd5a1da4ba4363c77ac2986bcc30ea8
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-06-16 15:35:06 +0430

    Fix top menu hover functionality.

--- inc/template/header.inc.php
+++ inc/template/header.inc.php
@@ -39,22 +39,73 @@
 							<span class="menu-items-font menu-items-bg">Services</span>
 							<span id="sub-menu-services">
 								<ul>
-									<li>
-										<span class="float-left">Network Management</span>
+									<li class="float-left">
+										<span>Network Management</span>
 										<ul class="sub-menu-services-sub-items">
 											<span class="float-left">
 												<li><a href="#">Network Servers(DNS, DHPCP, FTP, Mail server, Web server, ...)</a></li>
 												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
+												<li><a href="#">Applying Network Security Policies</a></li>
+												<li><a href="#">Network Monitoring and Management</a></li>
 											</span>
 											<span class="float-right">
-												<li><a href="#">Network Servers(DNS, DHPCP, FTP, Mail server, Web server, ...)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
-												<li><a href="#">Server OS(Linux, Windows)</a></li>
+												<li><a href="#">Clustering</a></li>
+												<li><a href="#">Internet Sharing</a></li>
+												<li><a href="#">Virtualization</a></li>
+												<li><a href="#">Voice Over Internet Protocol (VoIP)</a></li>
+											</span>
+											<br class="clear-both" />
+										</ul>
+									</li>
+									<li>
+										<span class="float-left">Security</span>
+										<ul class="sub-menu-services-sub-items">
+											<span class="float-left">
+												<li><a href="#">IPSec and SSL Virtual Private Network (VPN)</a></li>
+												<li><a href="#">Demilitrized Zone (DMZ), Firewall</a></li>
+												<li><a href="#">Insertion Detection and Prevention System (IPS, IDS)</a></li>
+												<li><a href="#">Autherization, Authentication, Acconting (AAA)</a></li>
+												<li><a href="#">Admin Auditing</a></li>
+											</span>
+											<span class="float-right">
+												<li><a href="#">Security Monitoring and Management</a></li>
+												<li><a href="#">DDOS Attack Protection</a></li>
+												<li><a href="#">Content Security</a></li>
+												<li><a href="#">Infrastructure Service Security</a></li>
+											</span>
+											<br class="clear-both" />
+										</ul>
+									</li>
+									<li>
+										<span class="float-left">Wireless</span>
+									</li>
+									<li>
+										<span class="float-left">Software</span>
+										<ul class="sub-menu-services-sub-items">
+											<span class="float-left">
+												<li><a href="#">Automation</a></li>
+												<li><a href="#">Intelligent Decision Dddddd System</a></li>
+												<li><a href="#">Desktop, Mobile, and Integrated Application</a></li>
+											</span>
+											<br class="clear-both" />
+										</ul>
+									</li>
+									<li>
+										<span class="float-left">Web Hosting</span>
+										<ul class="sub-menu-services-sub-items">
+											<span class="float-left">
+												<li><a href="#">Linux</a></li>
+												<li><a href="#">Windows</a></li>
+											</span>
+											<br class="clear-both" />
+										</ul>
+									</li>
+									<li>
+										<span class="float-left">Web Design</span>
+										<ul class="sub-menu-services-sub-items">
+											<span class="float-left">
+												<li><a href="#">Professinal Web Design</a></li>
+												<li><a href="#">Search Engine Optimization (SEO)</a></li>
 											</span>
 											<br class="clear-both" />
 										</ul>
