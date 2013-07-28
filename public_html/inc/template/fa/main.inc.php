aa69d403e0ef7618f94a3c0f6e3bee732e3ee725
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-07-28 22:06:14 +0430

    Add details for home page - main part. Fine tune the details of home page - main part.

--- public_html/inc/template/fa/main.inc.php
+++ public_html/inc/template/fa/main.inc.php
@@ -1,9 +1,8 @@
     <div id="main">
     	<div id="main-text">
         	<div>
-            	<div class="main-text-title">Duis aute irure dolor in reprehenderit in voluptate velit</div>
-            	<div class="main-text-description display-inline">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            	<div class="main-text-title">شرکت جهان پردازش البرز</div>
+            	<div class="main-text-description display-inline">شرکتی پویا و فعال میباشد و با سیاستگذاری وتعیین خط مشی و اهداف کلان توسط هیئت مدیره شرکت وبه دنبال آن پیشبرد اهداف و سیاست ها توسط مدیر عامل و سازمان شرکت با برخورداری و بهره گیری از تجربیات چندین ساله ی مجموعه ی مدیران ارشد و مجرب شرکت جهان پردازش البرز مدیریت میشود.</div>
                 <div class="main-show-more-button display-inline" style="display: none">Show More</div>
                 <div class="main-point" style="display: none">
                 	<div class="float-right learn-more-point">&nbsp;</div>
@@ -13,9 +12,9 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
                 </div>
                 <div id="products">
                 	<div class="float-left products-network">
-                    	<div class="products-title">NETWORK</div>
-                    	<div class="products-subtitle">MANAGEMENT</div>
-                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                    	<div class="products-title">شبکه</div>
+                    	<div class="products-subtitle">مدیریت شبکه</div>
+                        <div class="products-description">راه اندازی پشتیبانی و مدیریت انواع شبکه های بی سیم کابلی و فیبر نوری</div>
                         <div class="products-learn-more">
                         	<div class="float-left products-learn-more-l">&nbsp;</div>
                         	<div class="float-left products-learn-more-m">LEARN MORE</div>
@@ -24,9 +23,9 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
                         </div>
                     </div>
                     <div class="float-left products-programing">
-                    	<div class="products-title">PROGRAMING</div>
-                    	<div class="products-subtitle">SOFTWARE DEVELOPMENT</div>
-                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                    	<div class="products-title">برنامه نویسی</div>
+                    	<div class="products-subtitle">توسعه نرم افزاری</div>
+                        <div class="products-description">پیاده سازی راه حل های نرم افزاری برای توسعه و افزایش ضریب نفوذ در صنعت</div>
                         <div class="products-learn-more">
                         	<div class="float-left products-learn-more-l">&nbsp;</div>
                         	<div class="float-left products-learn-more-m">LEARN MORE</div>
@@ -35,9 +34,9 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
                         </div>
                     </div>
                     <div class="float-left products-hosting">
-                    	<div class="products-title">HOSTING</div>
-                    	<div class="products-subtitle">WEB AND APPLICATIONS</div>
-                        <div class="products-description">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</div>
+                    	<div class="products-title">میزبانی وب</div>
+                    	<div class="products-subtitle">وب هاستینگ در دیتاسنتر های بزرگ دنیا</div>
+                        <div class="products-description">بهترین هاست ها بر روی سرورهای قدرمتند که در دیتاسنترهای بزرگ دنیا نگهداری می شوند</div>
                         <div class="products-learn-more">
                         	<div class="float-left products-learn-more-l">&nbsp;</div>
                         	<div class="float-left products-learn-more-m">LEARN MORE</div>
