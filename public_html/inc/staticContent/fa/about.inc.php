91ff8401ef0d3668015d5ef0101debcd6afa583f
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-08-22 17:39:06 +0430

    Add about content.

--- public_html/inc/staticContent/fa/about.inc.php
+++ public_html/inc/staticContent/fa/about.inc.php
@@ -1,14 +1,18 @@
 	<div id="about-us">
 		<div class="pages-navigation-bar">
-			<div>You are here > Home > About Us</div>
+			<div>شما اینجا هستید &gt; <a class="smallerLatin" href="/">JPA</a> &gt; <a href="/about">درباره ما</a></div>
 		</div>
 		<div id="pages-main">
-			<div class="banner">&nbsp;</div>
+			<!-- <div class="banner">&nbsp;</div> -->
 			<div class="main-text">
-				<div class="title">vajhv jhajh jhag enkne ncg</div>
-				<div class="description">vajhv jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne n ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne ncg jhajh jhag enkne ncg</div>
+				<div class="title">شرکت جهان پردازش البرز (مسئولیت محدود)</div>
+				<div class="description">
+					<p>شرکتی پویا و فعال می باشد و با سیاست گذاری و تعیین خط مشی و اهداف کلان توسط هیئت مدیره شرکت و به دنبال آن پیشبرد اهداف و سیاست ها توسط مدیر عامل و سازمان شرکت با برخورداری و بهره گیری از تجربیات چندید ساله مجموعه ی مدیران ارشد و مجرب شرکت جهان پدرازش البرز مدیریت می شود. نمودار سازمانی این شرکت که نشانگر ساختار تشکیلاتی و دپارتمان های مختلف شرکت می باشد، در این بخش اراپه گردیده است. این شرکت به هدف اراپه خدمات در این بخش ها تاسیس گردیده است: مدیریت شبکه، امنیت، شبکه بیسیم، نرم افزار، طراحی وب و میزبانی وب.</p>
+					<p>شرکت جهان پردازش البرز با بهره گیری از مدیران و کارشناسان با تجربه و توانمند، هدایت و راهبری، سازماندهی، برنامه ریزی و کنترل زمان و هزینه، برآورد نیروی انسانی مورد نیاز، طرح ریزی کلی و عمومی جهت انجام امور مربوط به پروژه ها، اعمال کنترل های لازم بر جریان اجرای کار هماهنگی کلی سازمان های درگیر پروژه اعم از داخلی و خارجی، ایجاد ارتباط با کارفرما و جستوجو و تعیین پتانیسل های کاری را در زمینه های طرای مهندسی و اجرای طرح ها عهده دار می گردد.</p>
+				</div>
 			</div>
 		</div>
+		<!--
 		<div id="about-us-our-latest-projects">
 			<div>
 				<div class="float-left">
@@ -39,5 +43,6 @@
 				<br class="clear-both" />
 			</div>
 		</div>
+		-->
 	</div>
 
