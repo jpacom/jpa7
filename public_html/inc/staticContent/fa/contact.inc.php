7e8b121d7c47f68bdcab2b2f874581948354c31e
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-08-28 14:10:33 +0430

    Add contact us address and form.

--- public_html/inc/staticContent/fa/contact.inc.php
+++ public_html/inc/staticContent/fa/contact.inc.php
@@ -56,15 +56,52 @@
 		<div class="contact">
 			<div>
 				<div class="address-box float-left">
-					<div class="title">Karaj Office</div>
-					<div class="description">Unit 601. sixth floor. Eram Tower. Enqelab St. Hafte Tir. Karaj.</div>
-					<div class="description">Tell: +98 263 220 9245</div>
-					<div class="description">Fax: +98 263 228 4724</div>
+					<div class="title">دفتر کرج</div>
+					<div class="description">کرج - چهارراه هفت تیر ساختمان یاس طبقه ۵ واحد ۵۰۲</div>
+					<div class="description">تلفن: +98 263 220 9245</div>
+					<div class="description">تلفکس: +98 263 228 4724</div>
+					<!--
 					<br />
 					<div class="title">Tehran Office</div>
 					<div class="description">Unit 601. sixth floor. Eram Tower. Enqelab St. Hafte Tir. Karaj. Tell: +98 263 220 9245 Fax: +98 263 228 4724</div>
+					-->
 				</div>
-				<div class="contact-box float-right"></div>
+				<div class="contact-box float-right">
+					<form id="contact-us-form" method="post" target="">
+						<div class="float-left topleft">
+							<label class="float-left">نام:</label>
+							<input type="text" id="contact-us-form-firstname" name="contact-us-form-firstname" />
+							<br class="clear-both" />
+							<label class="float-left">نام خانوادگی:</label>
+							<input type="text" id="contact-us-form-lastname" name="contact-us-form-lastname" />
+							<br class="clear-both" />
+							<label class="float-left">پست الکترونیک:</label>
+							<input type="text" id="contact-us-form-email" name="contact-us-form-email" />
+							<br class="clear-both" />
+							<label class="float-left">تلفن:</label>
+							<input type="text" id="contact-us-form-phone" name="contact-us-form-phone" />
+						</div>
+						<div class="float-left topright">
+							<label class="float-left">شرکت:</label>
+							<input type="text" id="contact-us-form-company" name="contact-us-form-company" />
+							<br class="clear-both" />
+							<label class="float-left">دپارتمان:</label>
+							<input type="text" id="contact-us-form-department" name="contact-us-form-department" />
+							<br class="clear-both" />
+							<label class="float-left">کشور:</label>
+							<input type="text" id="contact-us-form-country" name="contact-us-form-country" />
+							<br class="clear-both" />
+						</div>
+						<div class="clear-both bottom">
+							<label class="float-left">پیغام:</label>
+							<textarea id="contact-us-form-message" name="contact-us-form-message"></textarea>
+							<br class="clear-both" />
+						</div>
+						<div class="clear-both bottombutton">
+							<input type="submit" value="ارسال" />
+						</div>
+					</form>
+				</div>
 				<br class="clear-both" />
 			</div>
 		</div>
