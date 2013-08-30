a04fd82f72a02c56f5b1329e9c45592cd85bf0f7
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-08-30 14:21:30 +0430

    Add meta description and keywords.

--- public_html/inc/template/page-start.inc.php
+++ public_html/inc/template/page-start.inc.php
@@ -2,13 +2,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title><?php
+<?php
 
 	$titles = array(
-		'/' => 'Home',
-		'/about' => 'About Us',
-		'/contact' => 'Contact Us',
+		'/' => 'صفحه اصلی',
+		'/about' => 'درباره ما',
+		'/contact' => 'تماس با ما',
 	);
+	
+	$descriptions = array(
+		'/' => 'جهان پردازش البرز (JPA7) در زمینه های طراحی وبسایت، برنامه نویسی، شبکه های بیسیم و با سیم، دوربین مدار بسته و وب هاستینگ آماده ارائه خدماتی بی نظیر می باشد.',
+		'/about' => 'جهان پردازش البرز با استفاده از کادری خلاق و جوان در پی نو آوری و ارائه راه حل های نو در بالاترین سطح ممکن می باشد.',
+		'/contact' => 'پیروزی شرکت جهان پردازش البرز در پی ایجاد محیطی برای پاسخگویی به نیاز های مشتریان با تکیه بر کادری پیشرو، جوان و خلاق به دست آمده است.',
+	);
+	
+	$keywords = array(
+		'/' => 'جهان پردازش البرز, طراحی حرفه ای وبسایت, برنامه نویسی, شبکه, بیسیم, وایرلس, هاست, دامنه, نرم افزار',
+		'/about' => 'جهان پردازش البرز, JPA7, طراحان حرفه ای, برنامه نویسان, پاسخگویی سریع',
+		'/contact' => 'جهان پردازش البرز, JPA7, پاسخگویی سریع, آدرس, ایمیل, تلفن, پیغام, تماس',
+	);
+
+	if(@ $descriptions[$url])
+	{
+?>
+<meta name="description" content="<?php echo $descriptions[$url]; ?>" />
+<?php
+	}
+	
+	if(@ $keywords[$url])
+	{
+?>
+<meta name="keywords" content="<?php echo $keywords[$url]; ?>" />
+<?php
+	}
+?>
+<title><?php
 	
 	if($url != '/')
 	{
@@ -16,7 +44,7 @@
 		echo " &mdash; ";
 	}
 
- ?>Jahan Pardazesh Alborz</title>
+ ?>جهان پردازش البرز (JPA7)</title>
 	<style type="text/css" media="screen">
 		@import "<?php echo 'http://images.' . $basedomain; ?>/stylesheet/fontfaces.css";
 		@import "<?php echo 'http://images.' . $basedomain; ?>/stylesheet/global.css";
