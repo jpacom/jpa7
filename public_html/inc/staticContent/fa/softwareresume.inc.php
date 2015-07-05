f2ac6fa59f3acfefe1382b72dde06a37167cff68
amir <amir.saiedmehr@gmail.com>
2015-07-05 16:53:33 +0430

    Add works pages.

---
+++ public_html/inc/staticContent/fa/softwareresume.inc.php
@@ -0,0 +1,87 @@
+	<div id="services">
+		<div class="pages-navigation-bar">
+			<div>شما اینجا هستید &gt; <a class="smallerLatin" href="/">JPA</a> &gt; نمونه کار &gt; <a href="/networkresume">نرم افزاری</a></div>
+		</div>
+		<div id="pages-main">
+			<!-- <div class="banner">&nbsp;</div> -->
+			<div class="main-text">
+        <div class="title">نمونه کارها</div>
+        <div>
+        <div class="tumbnail_image left_float" style="background-image: url(&quot;http://jpa7.local/inc/images/works/software.jpg&quot;);position:relative;width:600px;height:264px;background-repeat:no reapeat;margin-top:60px;margin-right:15px;"></div>
+          <p class="title_h2">نرم افزاری</p>
+          <p>وبسایت های طراحی شده : </p>
+          <ul class="list">
+
+            <li><a href="http://www.2tdk.com/">www.2tdk.com</a></li>
+            <li><a href="http://www.jalinusarya.com/">www.jalinusarya.com</a></li>
+            <li><a href="http://www.doonapol.ir/">www.doonapol.ir</a></li>
+            <li><a href="http://www.rta.ir/">www.rta.ir</a></li>
+            <li><a href="http://www.o-xe.com/">www.o-xe.com</a></li>
+            <li><a href="http://www.perstrans.com/">www.perstrans.com</a></li>
+            <li><a href="http://www.pizzagoodfellow.com/">www.pizzagoodfellow.com</a></li>
+            <li><a href="http://theeditor.co.nz/">www.theeditor.co.nz</a></li>
+            <li><a href="http://www.licoricefactory.com/">www.licoricefactory.com</a></li>
+            <li><a href="http://www.peysaze.ir/">www.peysaze.ir</a></li>
+            <li><a href="http://www.arsehconsulting.com/">www.arsehconsulting.com</a></li>
+            <li><a href="http://www.pinar-co.ir/">www.pinar-co.ir</a></li>
+            <li><a href="http://www.nanonet.ir/">www.nanonet.ir</a></li>
+            <li><a href="http://www.irannotebook.com/">www.irannotebook.com</a></li>
+            <li><a href="http://www.icdco.ir/">www.icdco.ir</a></li>
+            <li><a href="http://www.moalem1.ir/">www.moalem1.ir</a></li>
+            <li><a href="http://www.belkooh.com/">www.belkooh.com</a></li>
+            <li><a href="http://www.sadaftours.ir/">www.sadaftours.ir</a></li>
+            <li><a href="http://www.irmov.com/">www.irmov.com</a></li>
+            <li><a href="http://www.r-kayin.com/">www.r-kayin.com</a></li>
+            <li><a href="http://podologie-kaviar.de.qwd.ir/">www.podologie-kaviar.de.qwd.ir</a></li>
+            <li><a href="http://chargrill.co.nz/">www.chargrill.co.nz</a></li>
+            <li><a href="http://passargad-oil.com.qwd.ir/">www.passargad-oil.com.qwd.ir</a></li>
+            <li><a href="http://cdn.ime.co.ir/">www.cdn.ime.co.ir</a></li>
+            <li><a href="http://www.qwd.ir/">www.qwd.ir</a></li>
+            <li><a href="http://sharghcement.ir.qwd.ir/">www.sharghcement.ir.qwd.ir</a></li>
+            <li><a href="http://arian.ir4.ir/">www.arian.ir4.ir</a></li>
+            <li><a href="http://www.bimehamin-karafarin.ir/">www.bimehamin-karafarin.ir</a></li>
+            <li><a href="http://www.mci.ir/web/guest/home">www.mci.ir/web/guest/home</a></li>
+            <li><a href="http://rc.majlis.ir/fa/law">www.rc.majlis.ir/fa/law</a></li>
+            <li><a href="http://www.loghme.com/">www.loghme.com</a></li>
+            <li><a href="http://www.lavan-co.ir/">www.lavan-co.ir</a></li>
+            <li><a href="http://rc.majlis.ir/fa/lawLowheHagh.msi">www.rc.majlis.ir/fa/lawLowheHagh.msi</a></li>
+            <li><a href="http://www.mspars.org/">www.mspars.org</a></li>
+            <li><a href="http://parsamagham.com/">www.parsamagham.com</a></li>
+            <li><a href="http://ictbazar.com/">www.ictbazar.com</a></li>
+            <li><a href="http://dic.ir4.ir/">www.dic.ir4.ir</a></li>
+            <li><a href="http://www.darabgerd.ir/">www.darabgerd.ir</a></li>
+            <li><a href="http://f.ir4.ir/empty.htm">www.f.ir4.ir</a></li>
+            <li><a href="http://www.ir4.org/">www.ir4.org</a></li>
+            <li><a href="http://www.packtek.net/">www.packtek.net</a></li>
+            <li><a href="http://www.kahtmayan.com/">www.kahtmayan.com</a></li>
+            <li><a href="http://www.rici.ac.ir/">www.rici.ac.ir</a></li>
+            <li><a href="http://www.piicgroup.com/">www.piicgroup.com</a></li>
+            <li><a href="http://www.shahryarcohanzad.com/">www.shahryarcohanzad.com</a></li>
+            <li><a href="http://www.ier.ir/">www.ier.ir</a></li>
+            <li><a href="http://mail.majlis.ir/">www.mail.majlis.ir</a></li>
+            <li><a href="http://www.csr.ir/">www.csr.ir</a></li>
+            <li><a href="http://www.irannotebook.com/">www.irannotebook.com</a></li>
+            <li><a href="http://www.karasanat.com/">www.karasanat.com</a></li>
+            <li><a href="http://www.mellat.majlis.ir">www.mellat.majlis.ir</a></li>
+          </ul>
+          &nbsp;
+
+
+
+        </div>
+
+        <div style="clear: both;height: 10px;"></div>
+
+      </div>
+
+
+
+
+    </div>
+
+
+
+  </div>
+
+
+
