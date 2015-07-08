52004012e1c16e93cc1c7d9f6d3299b88fda3d39
amir <amir.saiedmehr@gmail.com>
2015-07-08 10:17:50 +0430

    Add second website portfolio page.

---
+++ public_html/inc/staticContent/fa/software-portfolio-page1.inc.php
@@ -0,0 +1,75 @@
+	<div id="services">
+		<div class="pages-navigation-bar">
+			<div>شما اینجا هستید &gt; <a class="smallerLatin" href="/">JPA</a> &gt; نمونه کار &gt; <a href="/software-portfolio">نرم افزاری</a></div>
+		</div>
+		<div id="pages-main">
+			<!-- <div class="banner">&nbsp;</div> -->
+			<div class="main-text">
+				<div class="title">نمونه کارها</div>
+				<p class="title_h2">نرم افزاری</p>
+				<div style="clear: both;height: 10px;"></div>
+				<div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/2tdk.jpg&quot;);">
+						<a href="http://www.2tdk.com/">www.2tdk.com</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/jalinusarya.jpg&quot;);">
+						<a href="http://www.jalinusarya.com/">www.jalinusarya.com</a>
+					</div>  
+					<div style="clear: both;height: 25px;"></div>  
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/doonapol.jpg&quot;);">
+						<a href="http://www.doonapol.ir/">www.doonapol.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/rta.jpg&quot;);">
+						<a href="http://www.rta.ir/">www.rta.ir</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/o-xe.jpg&quot;);">
+						<a href="http://www.o-xe.com/">www.o-xe.com</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/pizzagoodfellow.jpg&quot;);">
+						<a href="http://www.pizzagoodfellow.com/">www.pizzagoodfellow.com</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/theeditor.jpg&quot;);">
+						<a href="http://www.theeditor.co.nz/">www.theeditor.co.nz</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/licoricefactory.jpg&quot;);">
+						<a href="http://www.licoricefactory.com/">www.licoricefactory.com</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/peysaze.jpg&quot;);">
+						<a href="http://www.peysaze.ir/">www.peysaze.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/arsehconsulting.jpg&quot;);">
+						<a href="http://www.arsehconsulting.com/">www.arsehconsulting.com</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/pinar-co.jpg&quot;);">
+						<a href="http://www.pinar-co.ir/">www.pinar-co.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/nanonet.jpg&quot;);">
+						<a href="http://www.nanonet.ir/">www.nanonet.ir</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+
+				</div>
+				<div class="page_button_div">
+					<a href="/software-portfolio-page1"><p class="page_button active_button">1</p></a>
+					<a href="/software-portfolio-page2"><p class="page_button">2</p></a>
+					<a href="/software-portfolio-page3"><p class="page_button">3</p></a>
+				</div>
+				<div style="clear: both;height: 10px;"></div>
+
+			</div>
+
+
+
+
+		</div>
+
+
+
+	</div>
+
+
+
---
+++ public_html/inc/staticContent/fa/software-portfolio-page2.inc.php
@@ -0,0 +1,75 @@
+	<div id="services">
+		<div class="pages-navigation-bar">
+			<div>شما اینجا هستید &gt; <a class="smallerLatin" href="/">JPA</a> &gt; نمونه کار &gt; <a href="/software-portfolio">نرم افزاری</a></div>
+		</div>
+		<div id="pages-main">
+			<!-- <div class="banner">&nbsp;</div> -->
+			<div class="main-text">
+				<div class="title">نمونه کارها</div>
+				<p class="title_h2">نرم افزاری</p>
+				<div style="clear: both;height: 10px;"></div>
+				<div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/irannotebook.jpg&quot;);">
+						<a href="http://www.irannotebook.com/">www.irannotebook.com</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/icdco.jpg&quot;);">
+						<a href="http://www.icdco.ir/">www.icdco.ir</a>
+					</div>  
+					<div style="clear: both;height: 25px;"></div>  
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/moalem1.jpg&quot;);">
+						<a href="http://www.moalem1.ir/">www.moalem1.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/belkooh.jpg&quot;);">
+						<a href="http://www.belkooh.com/">www.belkooh.com</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/irmov.jpg&quot;);">
+						<a href="http://www.irmov.com/">www.irmov.com</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/r-kayin.jpg&quot;);">
+						<a href="http://www.r-kayin.com/">www.r-kayin.com</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/podologie-kaviar.jpg&quot;);">
+						<a href="http://www.podologie-kaviar.de.qwd.ir/">www.podologie-kaviar.de.qwd.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/chargrill.jpg&quot;);">
+						<a href="http://www.chargrill.co.nz/">www.chargrill.co.nz</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/sharghcement.jpg&quot;);">
+						<a href="http://www.sharghcement.ir.qwd.ir/">www.sharghcement.ir.qwd.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/arian.jpg&quot;);">
+						<a href="http://www.arian.ir4.ir/">www.arian.ir4.ir</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/passargad-oil.jpg&quot;);">
+						<a href="http://www.passargad-oil.com.qwd.ir/">www.passargad-oil.com.qwd.ir</a>
+					</div>
+					<div class="tumbnail_image left_float tumbnail_website_pic" style="background-image: url(&quot;http://jpa7.local/inc/images/websites/bimehamin-karafarin.jpg&quot;);">
+						<a href="http://www.bimehamin-karafarin.ir/">www.bimehamin-karafarin.ir</a>
+					</div> 
+					<div style="clear: both;height: 25px;"></div>
+
+				</div>
+				<div class="page_button_div">
+					<a href="/software-portfolio-page1"><p class="page_button">1</p></a>
+					<a href="/software-portfolio-page2"><p class="page_button active_button">2</p></a>
+					<a href="/software-portfolio-page3"><p class="page_button">3</p></a>
+				</div>
+				<div style="clear: both;height: 10px;"></div>
+
+			</div>
+
+
+
+
+		</div>
+
+
+
+	</div>
+
+
+
